25da1de0dcad7f756643ada67dd3d2fe877cc480
Sarah Roden <soumya1910@gmail.com>
2019-10-27 17:55:08 -0400

    Update data.py

--- word_language_model/data.py
+++ word_language_model/data.py
@@ -4,8 +4,26 @@ import torch
 
 class Corpus(object):
     def __init__(self, path):
-        self.dictionary = Dictionary()
-        self.train = self.tokenize(os.path.join(path, 'train.p'))
-        self.valid = self.tokenize(os.path.join(path, 'valid.p'))
-        self.test = self.tokenize(os.path.join(path, 'test.p'))
+        
+        train_graph = '/home/soumyar/dataExpOneTrain.p'
+        val_graph='/home/soumyar/dataExpOneVal.p'
+        test_graph='/home/soumyar/dataExpOneTest.p'
+
+        f = open(train_graph, 'rb')
+        data= load_func(f)
+        f.close()
+
+        f=open(val_graph,'rb')
+        data_val=load_func(f)
+        f.close()
+
+        f=open(test_graph, 'rb')
+        data_test=load_func(f)
+        f.close()
+        
+        ##reshape to be 1by 48 and lump them all together!!
+        
+        self.train = data
+        self.valid = data_val
+        self.test = data_test
 
